a81a385ccda9a5ab9822b712348a3293be4804e6
dev-rabin <robinmandhotia@gmail.com>
2023-07-21 21:10:30 +0530

    second commit

---
+++ src/models/user_model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const UserModel = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email : {
+        type:String,
+        // required : true
+    },
+    phoneNumber : {
+        type : String,
+        // required: true
+    }
+})
+
+module.exports = mongoose.model('Data', UserModel)
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const model = require('./../models/user_model')
 
-router.post('/post',(req,res) =>{
-    res.send('POST API')
+// Post Method
+router.post('/post',async (req,res) =>{
+    // res.send('POST API')
+    const data = new model({
+        name : req.body.name,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave);
+    } catch (error) {
+        res.status(400).json({message : error.message})
+    }
+})
+
+// Get all Method
+router.get('/getAll' ,(req,res) => {
+    res.send('Get All API')
+})
+
+// Get by ID Method
+router.get('/getOne/:id', (req,res) =>{
+    res.send('Get by ID API');
+})
+
+// Update by ID Method 
+router.patch('/update/:id', (req,res) =>{
+    res.send('Update by ID')
+})
+// Delte by ID 
+router.delete('/delete/:id', (req,res) =>{
+    res.send('Delete by id')
 })
 module.exports = router;
